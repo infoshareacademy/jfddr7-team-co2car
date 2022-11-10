a9290dd9f416c01cfa03fba44f21657faba828e3
Dorota Dawidowicz <dorotadawidowicz@gmail.com>
2022-11-10 11:53:24 +0100

    Home.tsx edited, fetch added; Navigation.tsx takes styles from the Material UI and displays logged in user
     Co-authored-by: Monika Hilbrycht <monika-hilbrycht@users.noreply.github.com>
    Co-authored-by: Grzegorz Uberman <g-uberman@users.noreply.github.com>
    Co-authored-by: tom-stenka <tom.stenka@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { Home } from "./components/Home";
 import { FC } from "react";
 import { Register } from "./components/Register/Register";
 import { Login } from "./components/Login/Login";
 import { Navigation } from "./components/Navigation";
-// import { Context } from "./ContextProvider";
+import { Context } from "./ContextProvider";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth, firebaseDb } from "./index";
 
+//-----!!---poprawic kod tak aby strona pamietała że uzytkownik jest zalogowany --> Dodałam useEffect z onAuthStateChanged który powieni robic to że strona zapamiętuje zalogowanego uzytkownika.
 export const App: FC = () => {
+  const navigate = useNavigate();
+  const { username, setUsername } = useContext(Context);
+
+  useEffect((): void => {
+    onAuthStateChanged(firebaseAuth, async (user) => {
+      if (user) {
+        const userEmail = user.email;
+        setUsername(userEmail || "");
+      } else {
+        setUsername("");
+      }
+    });
+  }, []);
+
+  //   useEffect((): void => {
+  //     if (username) {
+  //       navigate("/home");
+  //     } else {
+  //       navigate("/login");
+  //     }
+  //   }, [username]);
+
+  //-----!!---poprawic kod tak aby strona pamietała że uzytkownik jest zalogowany --> Dodałam wyżej useEffect z onAuthStateChanged który powieni robic to że strona zapamiętuje zalogowanego uzytkownika.
+
   return (
     <>
-      App
       <Navigation />
       <Routes>
         <Route path="/home" element={<Home />}></Route>
